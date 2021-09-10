7b1c6c21a4c7fc5de1b7b2294f38d133f4224b1c
trefis <trefis@pm.me>
2021-09-10 21:15:23 +0700

    🔄️ migrating from local repo

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../prisma/prisma";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>,
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // request example with prisma
+  await prisma.channels.findMany().then((response) => {
+    res.status(200).json({ data: response });
+  });
 }
