f01afd2933bf52ad1189134504facd19e399499a
trefis <trefis@pm.me>
2021-09-11 18:31:21 +0700

    🎨 colors update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,10 @@ module.exports = {
         500: "500ms",
       },
       colors: {
-        darkLight: "var(--color-dark-light)",
-        darkDark: "var(--color-dark-dark)",
+        darkGrey: "var(--color-dark-light)",
+        darkBlack: "var(--color-dark-dark)",
+        primary: colors.purple[500],
+        primaryLight: colors.purple[400],
         warning: colors.yellow[500],
         info: colors.indigo[600],
         success: colors.green[500],
