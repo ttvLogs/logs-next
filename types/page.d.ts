b069819dadb07fd3cb21276df4d0b3c5e13e4465
trefis <trefis@pm.me>
2021-09-17 00:13:58 +0700

    🛠️types update

--- types/page.d.ts
+++ types/page.d.ts
@@ -1,7 +1,25 @@
-import { NextPage } from 'next'
-import { ComponentType, ReactElement, ReactNode } from 'react'
+import { NextPage } from "next";
+import type { AppProps } from "next/app";
+import type { Page } from "./page";
+import { ComponentType, ReactElement, ReactNode } from "react";
 
-export type Page<P = {}> = NextPage<P> & {
-  getLayout?: (page: ReactElement) => ReactNode
-  layout?: ComponentType
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient;
+    }
+  }
+
+  export type Page<P = {}> = NextPage<P> & {
+    getLayout?: (page: ReactElement) => ReactNode;
+    layout?: ComponentType;
+  };
+
+  export type ChannelsResponse = {
+    ChannelID: string;
+  };
+
+  export type Props = AppProps & {
+    Component: Page;
+  };
 }
