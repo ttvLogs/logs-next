cff391ebcffd3d6ce3895931371ace53ea4b4bdb
trefis <trefis@pm.me>
2021-09-17 00:14:47 +0700

    🔺moved type in page.d.ts

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,13 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
-}
-
 let prisma: PrismaClient;
 
 if (typeof window === "undefined") {
