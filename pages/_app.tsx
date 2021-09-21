10bef56e9a635c1f2ed90cfac481ff40bed9b405
trefis <trefis@pm.me>
2021-09-21 20:32:32 +0700

    implemented loading component while loading on route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import Router from "next/router";
 import { Fragment, useState, useEffect } from "react";
 import { ThemeProvider } from "next-themes";
+import Loading from "../components/Loading";
 
 function MyApp({ Component, pageProps }: Props) {
   const getLayout = Component.getLayout ?? ((page) => page);
@@ -28,11 +29,11 @@ function MyApp({ Component, pageProps }: Props) {
   return (
     <>
       {loading ? (
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          Loading.......
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <Loading />
         </ThemeProvider>
       ) : (
-        <ThemeProvider attribute="class" defaultTheme="dark">
+        <ThemeProvider attribute="class" defaultTheme="system">
           <Layout>{getLayout(<Component {...pageProps} />)}</Layout>
         </ThemeProvider>
       )}
