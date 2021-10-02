eac6fd41385fd5cfeadd73712075e9178dae2821
trefis <trefis@pm.me>
2021-10-02 17:00:58 +0700

    ⚙️images hostnames in next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: { domains: ["static-cdn.jtvnw.net", "cdn.betterttv.net", "cdn.frankerfacez.com"] },
+};
