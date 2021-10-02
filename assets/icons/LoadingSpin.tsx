a8a1946e71788d886fec2ec610f2a5c1b77f52db
trefis <trefis@pm.me>
2021-10-02 17:01:10 +0700

    🔃loading icon

---
+++ assets/icons/LoadingSpin.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+
+export const LoadingSpin: FC<{ className?: string }> = ({ className = "" }) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx={12}
+        cy={12}
+        r={10}
+        stroke="white"
+        strokeWidth={4}
+      />
+      <path
+        className="opacity-75"
+        fill="#9BA0A8"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
+  );
+};
--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,1 +1,2 @@
 export * from "./GithubIcons";
+export * from "./LoadingSpin";
