eee6709378407b7ddff387711581d7391b8d723a
trefis <trefis@pm.me>
2021-10-06 23:55:34 +0700

    html escape, links parser

--- pages/api/channel/[channel]/user/[user].ts
+++ pages/api/channel/[channel]/user/[user].ts
@@ -11,25 +11,37 @@ export default async function handler(
 
   await prisma
     .$queryRawUnsafe<ttvUser_116738112[]>(
-      `SELECT Name, Message, Emotes, Color, Badges, Timestamp FROM ${table} WHERE SenderID = ${req.query.channel}`,
+      `SELECT Name, Message, Emotes, Color, Badges, Timestamp, isDeleted FROM ${table} WHERE SenderID = ${req.query.channel} ORDER BY Timestamp DESC;`,
     )
     .then((response) => {
       const parsed = response.map((item: ttvUser_116738112) => {
+        item.Message = item.Message.replaceAll("<", "&lt;").replaceAll(
+          ">",
+          "&gt;",
+        );
+        const urlRegex =
+          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
+        item.Message = item.Message.replace(
+          urlRegex,
+          '<a href="$&" target="_blank" rel="noopener norefferer" className="mr-1">$&</a>',
+        );
         if (item.Emotes !== null) {
           const emotesArray: emote[] = JSON.parse(item.Emotes);
           emotesArray.forEach((emote: emote) => {
             item.Message = item.Message.replaceAll(
               emote.code,
-              `<img src="https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/1.0" alt="${emote.code}" className="w-6 h-6"/> `,
+              `<img src='https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/1.0' alt='sub emote' className='mx-1'/>`,
             );
           });
         }
+        console.log(item);
         return {
           name: item.Name,
-          color: item.Color,
+          color: item.Color === null ? "white" : item.Color,
           message: item.Message,
           timestamp: item.Timestamp,
-          badges: item.Badges === null ? "" : JSON.parse(item.Badges),
+          badges: item.Badges === null ? null : JSON.parse(item.Badges),
+          deleted: item.isDeleted === 0 ? false : true,
         };
       });
       res.status(200).json({ type: "ok", sender: "api", data: parsed });
