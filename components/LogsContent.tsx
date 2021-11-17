6261b6efe0e70445ba6f220ce0f17e90a6f5ec80
trefis <trefis@pm.me>
2021-11-17 22:24:58 +0700

    🚧 date format fix

--- components/LogsContent.tsx
+++ components/LogsContent.tsx
@@ -23,7 +23,6 @@ export const LogsContent: FC<ContentProps> = ({ channel, user }) => {
       <Alert type="error" title="Error while fetching data from the API" />
     );
   }
-  console.log(data);
   return (
     <section className="bg-gray-50 dark:bg-darkGrey rounded-lg h-full border dark:border-gray-500 mb-6 flex flex-col space-y-2 px-4 py-2">
       {data.data.map((item: log, index: number) => {
@@ -31,7 +30,7 @@ export const LogsContent: FC<ContentProps> = ({ channel, user }) => {
         return (
           <p className="flex space-x-2 flex-shrink-0" key={index}>
             <span className="flex-nowrap truncate flex-shrink-0 text-gray-600 dark:text-gray-400">
-              {format(date, "yyyy-mm-dd, h:MM:ss")}
+              {format(date, "yyyy-MM-dd, hh:mm:ss")}
             </span>
             <span className="font-medium flex" style={{ color: item.color }}>
               {item.name}:
