a3cdbf46d29c74e354914e558a4ac2ea3235d5c9
trefis <trefis@pm.me>
2021-11-18 19:08:39 +0700

    ⚙️7tv API response type

--- types/page.d.ts
+++ types/page.d.ts
@@ -99,4 +99,32 @@ declare global {
     images: { "1x": string; "2x": string; "3x": string };
     imageType: string;
   }[];
+
+  type channel7TVResponse = {
+    id: string;
+    name: string;
+    owner: {
+      id: string;
+      twitch_id: string;
+      login: string;
+      display_name: string;
+      role: {
+        id: string;
+        name: string;
+        position: number;
+        color: number;
+        allowed: number;
+        denied: number;
+        default: boolean;
+      };
+    };
+    visibility: number;
+    visibility_simple?: string[];
+    mime: string;
+    status: number;
+    tags?: string[];
+    width: number[];
+    height: number[];
+    urls: string[][];
+  }[];
 }
