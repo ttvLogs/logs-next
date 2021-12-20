8589b55cae67e705a8c8c3ab0e12ccec56d55736
trefis <trefis@pm.me>
2021-12-20 21:23:39 +0700

    ⚙️update configs after the yarn upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
       outline: {
-        primary: `2px dashed ${colors.purple[500]}`,
+        primary: `2px dashed ${colors.violet[500]}`,
       },
       outlineOffset: {
         0: "0",
@@ -18,12 +17,15 @@ module.exports = {
       colors: {
         darkGrey: "var(--color-dark-light)",
         darkBlack: "var(--color-dark-dark)",
-        primary: colors.purple[500],
-        primaryLight: colors.purple[400],
-        warning: colors.yellow[500],
+        primary: colors.violet[500],
+        primaryLight: colors.violet[400],
+        warning: colors.amber[500],
         info: colors.indigo[600],
-        success: colors.green[500],
+        success: colors.emerald[500],
         error: colors.red[500],
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet,
       },
     },
   },
